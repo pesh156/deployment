4b915a35b211647b35e8fd1544c7cb58515bd876
pesh156 <peshilapramodi60@gmail.com>
2026-07-19 13:20:17 +0530

    feat: update greeting message in App component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <h1>Hello DevOps</h1>
           <h2>Here is Your Sosamala</h2>
           <h1>Vite + React</h1>
-          <h1>Halo sudu mahattaya</h1>
+          <h1>Halo</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
